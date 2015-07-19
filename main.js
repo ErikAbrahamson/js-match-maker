059e8a73d0ff569fff161462d64f3bace2123812
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-19 16:31:01 -0600

    Add function to get student information

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-//Init JS Match Maker assignment
 console.log('sanity check!');
+// Init both student and mentor object array
+var students = [], mentors = [];
+// Pushes new student data to student object array
+function getStudent() {
+  var name = prompt('What is the student\'s name?');
+  var number = prompt('What is the student\'s phone #?');
+  var city = prompt('In what city does this student currently live?');
+
+  var studentInfo = {
+    name: '',
+    number: '',
+    city: ''
+  };
+
+  studentInfo.name = name;
+  studentInfo.number = number;
+  studentInfo.city = city;
+  students.push(studentInfo);
+
+  var newStudent = prompt('Do you want to add another student?');
+  if (newStudent.toLowerCase() === 'yes') {
+    getStudent();
+  }
+}
